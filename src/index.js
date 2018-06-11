4978753cfe8fb837105e0e54f4458a59480bea2b
KevenLeMoing <keven@fundshop.fr>
2018-06-11 11:45:00 +0200

    Initial commit with node modules created

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+	render(){
+		//ON peut écrire du html directement à cet endroit
+		<h1>Coucou depuis ma première fonction</h1>
+	}	
+}
+
+
+
+//////////////////////////////////////////////OLD CODE
+// Exemple de popup
+//alert ('Petite pop-bla')
