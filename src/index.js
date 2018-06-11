0ab0b1f904a2eab4f125660e92d7103848e2b25e
KevenLeMoing <keven@fundshop.fr>
2018-06-11 12:17:20 +0200

    add a gitignore + implement main layout in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { render } from 'react-dom';
+//CSS
+import './style/css/bootstrap.min.css';
+import './index.css';
 
 class App extends React.Component {
 	render(){
-		//ON peut écrire du html directement à cet endroit
-		<h1>Coucou depuis ma première fonction</h1>
+		return(
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-6">
+						<textarea value="Write your text here !" rows="35" className="form-controll">
+
+						</textarea>
+					</div>
+
+					<div className="col-sm-6">
+						<h1>Results</h1>
+					</div>
+				</div>
+			</div>
+
+			)
 	}	
 }
 
+render (
+	<App />,
+	document.getElementById('root')
+)
 
 
 //////////////////////////////////////////////OLD CODE
